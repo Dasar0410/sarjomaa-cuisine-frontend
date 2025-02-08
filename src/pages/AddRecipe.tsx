950c99f06565a8cb3ef759030b30920afaea24da
Dasar0410 <danielsarjomaa@gmail.com>
2025-02-08 13:50:04 +0100

    form structure for posting recipes

---
+++ src/pages/AddRecipe.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useState } from 'react';
+import NavigationBar from '../components/NavigationBar';
+import { addRecipe } from '../services/recipeService';
+import { Recipe } from '../types/recipe';
+
+function NewRecipe() {
+  const [recipe, setRecipe] = useState<Recipe>({
+      id: 100,
+      created_at: new Date().toISOString(),
+      creator: 0,
+      title: "",
+      description: "",
+      ingredients: [],
+      steps: [],
+      cuisine: "",
+      image_url: "",
+  });
+
+function handleSubmit(event: FormEvent) {
+  event.preventDefault();
+  
+  addRecipe(recipe);
+  
+
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipe({ ...recipe, [e.target.name]: e.target.value });
+  };
+  
+
+  return (
+    <div>
+      <NavigationBar />
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="title" value={recipe.title} onChange={handleChange} placeholder="Title" required className="border p-2 w-full" />
+      <input type="description" value={recipe.description} onChange={handleChange} placeholder="Description" required className="border p-2 w-full" />
+      <input type="text" name="cuisine" value={recipe.cuisine} onChange={handleChange} placeholder="Cuisine" required className="border p-2 w-full" />
+      <input type="text" name="image_url" value={recipe.image_url} onChange={handleChange} placeholder="Image URL" required className="border p-2 w-full" />
+        <button type="submit">Add Recipe</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewRecipe;
